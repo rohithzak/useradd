68361c5b392841074998ab0ef393860a2d22591c
rohithzak <rohithsekahar@gmail.com>
2023-10-17 19:19:35 +0530

    first commit

---
+++ useradd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+usernames=("ryan")
+
+for username in "${usernames[@]}"; do
+    home_dir="/home/$username"
+
+    sudo useradd -m -d "$home_dir" -s /bin/bash "$username"
+
+    cd "$home_dir"
+
+    sudo -u "$username" ssh-keygen -t rsa -b 2048 -f "$home_dir/.ssh/id_rsa" -N ""
+
+    sudo -u "$username" cp "$home_dir"/.ssh/id_rsa.pub "$home_dir"/.ssh/authorized_keys
+
+    echo "User '$username' added with home directory '$home_dir'."
+done
+
